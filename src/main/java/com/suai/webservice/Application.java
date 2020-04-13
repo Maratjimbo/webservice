0f3a77ae2e77b6658e9554d86b8495fd34bb6446
mialyshev <43436251+mialyshev@users.noreply.github.com>
2020-04-14 00:57:21 +0300

    Update Application.java

--- src/main/java/com/suai/webservice/Application.java
+++ src/main/java/com/suai/webservice/Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        //test for issue
+        //test for issue 1
     }
 }
